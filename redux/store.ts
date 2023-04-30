71d1a19b91f1df9fe3432c17e2c5c432285d5649
emmajane1313 <admin@digitalax.xyz>
2023-04-29 20:56:30 -0400

    dispatch set up

---
+++ redux/store.ts
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const reducer = combineReducers({
+});
+
+export const store = configureStore({
+  reducer: {
+    app: reducer,
+  },
+  middleware: (getDefaultMiddleware: any) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
