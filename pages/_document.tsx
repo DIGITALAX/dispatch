dd990a16371f82916406ac35fb0bdccdf0dced9f
emmajane1313 <admin@digitalax.xyz>
2023-05-03 05:37:25 -0400

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,126 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="og:url" content="https://dispatch.chromadin.xyz/" />
+        <meta name="og:title" content="Chromadin Dispatch" />
+        <meta name="og:description" content="Chromadin Creator Dispatch." />
+        <meta
+          name="og:image"
+          content="https://dispatch.chromadin.xyz/card.png/"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="og:url" content="https://dispatch.chromadin.xyz/" />
+        <meta
+          name="og:image"
+          content="https://dispatch.chromadin.xyz/card.png/"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@igitalax_" />
+        <meta name="twitter:creator" content="@digitalax_" />
+        <meta
+          name="twitter:image"
+          content="https://dispatch.chromadin.xyz/card.png/"
+        />
+        <meta name="twitter:url" content="https://dispatch.chromadin.xyz/" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="canonical" href="https://chromadin.xyz/" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/ArcadeClassic.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/DSDigi.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/EarlsRevenge.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Geometria.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/EconomicaRegular.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/EconomicaBold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+          type="font/ttf"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              @font-face {
+                font-family: "Arcade Classic";
+                font-weight: 400;
+                src: url("./fonts/ArcadeClassic.ttf");
+              }
+
+              @font-face {
+                font-family: "Geometria";
+                font-weight: 400;
+                src: url("./fonts/Geometria.ttf");
+              }
+              
+              @font-face {
+                font-family: "DS Digital";
+                font-weight: 400;
+                src: url("./fonts/DSDigi.ttf");
+              }
+              
+              @font-face {
+                font-family: "Earls Revenge";
+                font-weight: 400;
+                src: url("./fonts/EarlsRevenge.ttf");
+              }
+
+              @font-face {
+                font-family: "Economica R";
+                font-weight: 400;
+                src: url("./fonts/EconomicaRegular.ttf");
+              }
+              
+              @font-face {
+                font-family: "Economica B";
+                font-weight: 400;
+                src: url("./fonts/EconomicaBold.ttf");
+              }
+            `,
+          }}
+        ></style>
+      </Head>
       <body>
+        <script>0</script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
