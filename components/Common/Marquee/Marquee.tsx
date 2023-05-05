98a8d199925c1d1b80b30e7bd3e065220ca12809
emmajane1313 <admin@digitalax.xyz>
2023-05-05 04:00:29 -0400

    marquee overflow

--- components/Common/Marquee/Marquee.tsx
+++ components/Common/Marquee/Marquee.tsx
@@ -3,11 +3,11 @@ import MarqueeText from "react-fast-marquee";
 
 const Marquee: FunctionComponent = (): JSX.Element => {
   return (
-    <div className="relative w-full h-10 border-y border-white py-3 flex flex-row bottom-0">
-      <MarqueeText gradient={false} speed={70} direction={"right"}>
+    <div className="relative w-full h-10 border-y border-white py-3 flex flex-row bottom-0 overflow-hidden">
+      <MarqueeText gradient={false} speed={70} direction={"right"} className="overflow-hidden">
         {Array.from({ length: 30 }).map((_, index: number) => {
           return (
-            <span className="relative font-arcade text-sm text-white px-5" key={index}>
+            <span className="relative font-arcade text-sm text-white px-5 overflow-hidden" key={index}>
               CHROMADIN DISPATCH
             </span>
           );
