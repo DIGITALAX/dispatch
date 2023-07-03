ab549b861d0501170eecc95c0de52c1add71fd6f
emmajane1313 <admin@digitalax.xyz>
2023-07-03 12:01:19 -0400

    new video

--- redux/reducers/mainVideoSlice.ts
+++ redux/reducers/mainVideoSlice.ts
@@ -10,7 +10,7 @@ export interface MainVideoState {
 }
 
 const initialMainVideoState: MainVideoState = {
-  id: "0x01c6a9-0x37",
+  id: "0x01c6a9-0x38",
   collected: false,
   liked: false,
   mirrored: false,
